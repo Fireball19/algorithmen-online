3f9f8671c2a32afdcc200105b1fdeacee8e012cf
Fireball19 <fireball1199@googlemail.com>
2021-05-28 13:52:48 +0200

    Add util js file

---
+++ js/util.js
@@ -0,0 +1,43 @@
+var array = document.getElementById("array");
+  
+function generateArray() {
+	// delete old array
+	array.innerHTML = '';
+    for (var i = 0; i < 15; i++) {
+        var value = randomNumber();
+  
+        // create div for element
+        var arrayElement = document.createElement("div");
+  
+        // add style and label to element
+		arrayElement.classList.add("array-element");
+        arrayElement.style.height = `${30}px`;
+        arrayElement.style.transform = `translate(${i * 30}px)`;
+        var arrayElementLabel = document.createElement("label");
+        arrayElementLabel.classList.add("array-element-id");
+        arrayElementLabel.innerText = value;
+  
+        arrayElement.appendChild(arrayElementLabel);
+        array.appendChild(arrayElement);
+    }
+}
+
+function randomNumber() {
+	return Math.ceil(Math.random() * 100);
+}
+  
+function swap(element1, element2) {
+    return new Promise((resolve) => {
+        var tmp = element1.style.transform;
+        element1.style.transform = element2.style.transform;
+        element2.style.transform = tmp;
+  
+        window.requestAnimationFrame(function() {
+            // wait for 0.5 seconds
+            setTimeout(() => {
+                array.insertBefore(element2, element1);
+                resolve();
+            }, 500);
+        });
+    });
+}
