b537cc39ec8310ece635c353265e64c4577c5805
Fireball19 <fireball1199@googlemail.com>
2021-06-04 16:50:24 +0200

    Add double hashing algorithm

---
+++ js/hashing/doublehashing.js
@@ -0,0 +1,46 @@
+async function doubleHashing(delay = 750) {
+	// generate hashtable with random elements
+	generateHashtable(13);
+	
+    var hashtable = document.querySelectorAll(".hashtable-element");
+	
+	spinningDemo();
+	
+	var array = [];
+	for (var i = 0; i < 10; i++) {
+		array.push(randomNumber());
+	}
+	
+	for (var j = 0; j < 10; j++) {
+		var eingefuegt = false;
+		var counter = 0;
+		while (eingefuegt == false) {
+			var h0 = mod(array[j], 13);
+			var h1 = 1 + mod(array[j], 11);
+			var pos = mod(h0 - counter * h1, 13);
+		
+			// change the color of elements to comparing color
+			hashtable[pos].style.backgroundColor = "#CC6677";
+	  
+			// wait for .75 seconds
+			await new Promise((resolve) =>
+				setTimeout(() => {
+					resolve();
+				}, delay)
+			);
+			
+			if (hashtable[pos].childNodes[0].innerHTML == '') {
+				hashtable[pos].childNodes[0].innerHTML = array[j]; 
+				// change the color of compared elements to hashed color
+				hashtable[pos].style.backgroundColor = "#117733";
+				eingefuegt = true;
+			} else {
+				// change the color of compared elements to previous (hashed) color
+				hashtable[pos].style.backgroundColor = "#117733";
+				counter++;
+			}
+		}
+	}
+	
+	stopSpinningDemo();
+}
