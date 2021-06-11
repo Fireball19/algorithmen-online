9620108a88bc506670816b28b96f2d9fc8c57bff
Fireball19 <fireball1199@googlemail.com>
2021-06-11 22:42:54 +0200

    Add log to bubblesort

--- js/sorting/bubblesort.js
+++ js/sorting/bubblesort.js
@@ -1,3 +1,5 @@
+var log = document.getElementById("demo-log");
+
 async function bubbleSort(delay = 250) {
 	// generate array with random elements
 	generateArray("array");
@@ -6,9 +8,19 @@ async function bubbleSort(delay = 250) {
 	
 	spinningDemo();
 	
+	var runs = 1;
+	
 	var vertauscht;
 	var n = array.length;
 	do {
+		// log runs
+		if (runs > 1) {
+			log.innerHTML += '<br>'
+		}
+		log.innerHTML += '<strong>Durchlauf</strong>: ' + runs;
+		log.innerHTML += '<hr>'
+		runs++;
+		
 		vertauscht = false;
 		for (var i = 0; i < n - 1; i++) {
 			// change the color of elements to comparing color
@@ -25,11 +37,20 @@ async function bubbleSort(delay = 250) {
 			var value1 = Number(array[i].childNodes[0].innerHTML);
 			var value2 = Number(array[i + 1].childNodes[0].innerHTML);
 	  
+			// log comparing
+			log.innerHTML += '<strong>i: </strong>' + i + '&emsp;<strong>Vergleiche</strong>: ' + value1 + ' > ' + value2;
+			
 			if (value1 > value2) {
+				// log swap
+				log.innerHTML += '&emsp;<strong>Tausche</strong>: ' + value1 + ' und ' + value2;
+				
 				await swap(array[i], array[i + 1]);
 				array = document.querySelectorAll(".array-element");
 				vertauscht = true;
 			}
+			
+			// next line in log
+			log.innerHTML += '<br/>';
 	  
 			// change the color of compared elements to previous color
 			array[i].style.backgroundColor = "#88CCEE";
